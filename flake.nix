d8736cc496a2050f02c0ca3e8597dde614213de2
John Jorgensen <john@2gn.com>
2026-02-24 15:08:04 -0500

    Pin to upstream release and update lock

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     pi-mono = {
-      url = "github:badlogic/pi-mono";
+      url = "github:badlogic/pi-mono?ref=v0.54.2";
       flake = false;
     };
   };
