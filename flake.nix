b4517c95f6fa41d9dc7d17d7f6c827477df1c155
John Jorgensen <john@2gn.com>
2026-06-08 17:08:34 -0400

    chore: update pi-mono to v0.79.0

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     pi-mono = {
-      url = "github:earendil-works/pi?ref=v0.78.1";
+      url = "github:earendil-works/pi?ref=v0.79.0";
       flake = false;
     };
   };
--- nix/workspace.nix
+++ nix/workspace.nix
@@ -29,7 +29,7 @@ buildNpmPackage {
   # Set with fake hash first; nix will print the correct hash on first build.
   # Replace this with that value before upstreaming.
   # npmDepsHash = lib.fakeHash;
-  npmDepsHash = "sha256-7zLvhuhyOD/p5xCZ4hsCdFH9E9AQFu8HCOsjSxMB74o=";
+  npmDepsHash = "sha256-8Axh/EfrzQmR8ECezxI5t4NVwM+I00V5f4c02TMr/ZM=";
   npmDepsFetcherVersion = 2;
 
   # Build all workspace packages in repo-defined order.
